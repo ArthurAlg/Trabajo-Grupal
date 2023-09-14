482b028ffc78841bfce0fc8dd30ceca82932c528
ArthurAlg <aliagaarturo2004@gmail.com@>
2023-09-14 09:34:08 -0500

    Ultimos MVC

---
+++ Models/Carrito.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabajo_Grupal.Models
+{
+    public class Carrito
+    {
+        
+    }
+}
