b685cb6d27f62ebfb1b67fe7bfad15441f699e1a
yanne23 <yannella_lopez@usmp.pe>
2023-09-29 00:52:03 -1200

    nav y fotter (terminado)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function show(){
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
+}
