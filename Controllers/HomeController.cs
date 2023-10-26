f0eb751bae9c13ba366dd9315fe133bf0f6f918c
ArthurAlg <aliagaarturo2004@gmail.com@>
2023-10-26 13:48:45 -0500

    Avance del inicio (detalles y demas)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,11 +26,62 @@ public class HomeController : Controller
         _userManager = userManager;
         _signInManager = signInManager;
     }
-    public IActionResult Index()
-{
+    public IActionResult Index(){
     var productos = _context.DataProductos.Where(libro => libro.Precio < 70M).ToList();
     return View(productos);
-}
+    }
+
+    public async Task<IActionResult> DetailsHome(int? id){
+        Producto? objProduct = await _context.DataProductos.FindAsync(id);
+        if(objProduct == null){
+            return NotFound();
+        }
+        return View(objProduct);
+    }
+
+    public async Task<IActionResult> Add(int? id){
+        var userID = _userManager.GetUserName(User);
+        if(userID == null){
+            //no se ha logueado
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+            List<Producto> productos = new List<Producto>();
+            return  View("Index",productos);
+        }else{
+            //ya esta logueado
+            var producto = await _context.DataProductos.FindAsync(id);
+
+            Proforma proforma = new Proforma();
+            proforma.Producto = producto;
+            proforma.Precio = producto.Precio; //precio del producto en ese momento
+            proforma.Cantidad = 1;
+            proforma.UserID = userID;
+            _context.Add(proforma);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+    public async Task<IActionResult> Add1(int? id){
+        var userID = _userManager.GetUserName(User);
+        if(userID == null){
+            //no se ha logueado
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+            List<Producto> productos = new List<Producto>();
+            return  View("Index",productos);
+        }else{
+            //ya esta logueado
+            var producto = await _context.DataProductos.FindAsync(id);
+
+            MiLista milista = new MiLista();
+            milista.imgProducto = producto.ImageName;
+            milista.Producto = producto;
+            milista.Precio = producto.Precio; //precio del producto en ese momento
+            milista.UserID = userID;
+            _context.Add(milista);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
 
     public IActionResult Privacy()
     {
