b10744700ffa447b4761092f320482a0330be4f0
ArthurAlg <aliagaarturo2004@gmail.com@>
2023-11-09 16:00:50 -0500

    REST

--- Controllers/Rest/ProductoApiController.cs
+++ Controllers/Rest/ProductoApiController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Trabajo_Grupal.Data;
+using Trabajo_Grupal.Models;
+using Trabajo_Grupal.Service;
 
 namespace Trabajo_Grupal.Controllers.Rest
 {
@@ -10,6 +14,48 @@ namespace Trabajo_Grupal.Controllers.Rest
     [Route("api/[controller]")]
     public class ProductoApiController : ControllerBase
     {
-        
+        private readonly ProductoService _productoService;
+
+        public ProductoApiController(ProductoService productoService)
+        {
+            _productoService = productoService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<Producto>>> List()
+        {
+            var productos = await _productoService.GetAll();
+            if(productos == null)
+                return NotFound();
+            return Ok(productos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Producto>> GetProducto(int? id)
+        {
+            var producto = await _productoService.Get(id);
+            if(producto == null)
+                return NotFound();
+            return Ok(producto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Producto>> CreateProducto(Producto producto){
+            if (producto == null)
+            {
+                return BadRequest();
+            }
+            await _productoService.Crear(producto);
+            return Ok(producto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProducto(int? id)
+        {
+            await _productoService.Delete(id);
+            return Ok();
+        }
     }
 }
